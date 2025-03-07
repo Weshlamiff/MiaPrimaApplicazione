5398a1761c6a5b5fe0cef53d33f9e2bccbefd6d8
Weshlamiff <st10561@iismarconipieralisi.it>
2025-03-07 11:10:55 +0100

    layout fixato e aggiunta del log

--- app/src/main/java/com/example/miaprimaapplicazione/Register.java
+++ app/src/main/java/com/example/miaprimaapplicazione/Register.java
@@ -1,6 +1,13 @@
 package com.example.miaprimaapplicazione;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +17,26 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+
+        //Collegamento con elementi grafici
+        EditText editTextNome = findViewById(R.id.txt_nome);
+        EditText editTextCognome = findViewById(R.id.txt_cognome);
+        CalendarView calendar = findViewById(R.id.calendar);
+        EditText editTextUser = findViewById(R.id.txt_user);
+        EditText editTextPassword = findViewById(R.id.txt_password);
+        Button btnRegister = findViewById(R.id.btn_register);
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //uso del log
+                Log.d("Registrazione", editTextNome.getText().toString() + " " + editTextCognome.getText().toString());
+                Toast.makeText(Register.this, editTextNome.getText().toString() + " " + editTextCognome.getText().toString() + " registrato correttamente", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
