a68c2f551bc407e364ccfad885cb0d0886d0bbf8
Weshlamiff <st10561@iismarconipieralisi.it>
2025-03-14 11:26:12 +0100

    Creazione intent che parte dalla form register e presa dei dati dalla form login.

--- app/src/main/java/com/example/miaprimaapplicazione/Login.java
+++ app/src/main/java/com/example/miaprimaapplicazione/Login.java
@@ -2,10 +2,15 @@ package com.example.miaprimaapplicazione;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
 
 public class Login extends AppCompatActivity {
 
@@ -13,5 +18,31 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Intent intentreg = getIntent();
+        String utenteJson = intentreg.getStringExtra("utente");
+        Gson gson = new Gson();
+        Utente utente = gson.fromJson(utenteJson, Utente.class);
+
+        EditText editTextUser = findViewById(R.id.txt_Username);
+        EditText editTextPassword = findViewById(R.id.txt_Password);
+        Button btnLogin = findViewById(R.id.buttonLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(utente == null){
+                    Toast.makeText(Login.this, "Utente non trovato", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    if (editTextUser.getText().toString().equals(utente.getUsername()) && editTextPassword.getText().toString().equals(utente.getPassword())) {
+                        Toast.makeText(Login.this, "Login effettuato correttamente", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(Login.this, "Username o password errati", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/miaprimaapplicazione/Register.java
+++ app/src/main/java/com/example/miaprimaapplicazione/Register.java
@@ -1,5 +1,6 @@
 package com.example.miaprimaapplicazione;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.gson.Gson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,10 +34,28 @@ public class Register extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Utente utente = new Utente(editTextNome.getText().toString(), editTextCognome.getText().toString(),editTextUser.getText().toString(), editTextPassword.getText().toString());
-                //uso del log
-                Log.d("Registrazione", utente.getNome() + " " + utente.getCognome() +  " " + utente.getUsername() + " " + utente.getPassword());
-                Toast.makeText(Register.this, editTextNome.getText().toString() + " " + editTextCognome.getText().toString() + " registrato correttamente", Toast.LENGTH_LONG).show();
+                if (editTextNome.getText().toString().isEmpty() || editTextCognome.getText().toString().isEmpty() || editTextUser.getText().toString().isEmpty() || editTextPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(Register.this, "Compilare tutti i campi", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else if (editTextPassword.getText().toString().length() < 8) {
+                    Toast.makeText(Register.this, "La password deve avere almeno 8 caratteri", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else{
+                    Utente utente = new Utente(editTextNome.getText().toString(), editTextCognome.getText().toString(),editTextUser.getText().toString(), editTextPassword.getText().toString());
+                    Gson gson = new Gson();
+                    String utenteJson = gson.toJson(utente);
+                    Log.d("utenteJson", utenteJson);
+
+                    Intent intent = new Intent(Register.this, Login.class);
+                    intent.putExtra("utente", utenteJson);
+                    startActivity(intent);
+
+                    //uso del log
+                    Log.d("Registrazione", utente.getNome() + " " + utente.getCognome() +  " " + utente.getUsername() + " " + utente.getPassword());
+                    Toast.makeText(Register.this, editTextNome.getText().toString() + " " + editTextCognome.getText().toString() + " registrato correttamente", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
